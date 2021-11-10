09b4fad186f187608336e5ca84dcb1af78a8dc6d
Md Hasibul Islam <hasiee8004@gmail.com>
2021-11-10 15:22:47 +0600

    database updated

--- index.js
+++ index.js
@@ -4,14 +4,43 @@ const app = express();
 require("dotenv").config();
 app.use(express.json());
 app.use(cors());
-
 const port = process.env.PORT || 5000;
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xllaf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("HousePlant");
+    const servicesCollections = database.collection("HousePlantServices");
+    const userCollections = database.collection("HousePlantUsers");
 
 
-app.get('/', (req,res) => {
-    res.send("HOUSEPLANT Server is running")
-})
+    app.get('/services', async(req,res) => {
+        const cursor = servicesCollections.find({})
+        const services = await cursor.toArray()
+        res.send(services)
+    })
+    
+  } finally {
+    // client.close();
+  }
+}
 
-app.listen(port, ()=> {
-    console.log("server is running on port:", port);
-})
+run().catch(console.log("something error"));
+
+app.get("/", (req, res) => {
+  res.send("HOUSEPLANT Server is running");
+});
+
+app.listen(port, () => {
+  console.log("server is running on port:", port);
+});
